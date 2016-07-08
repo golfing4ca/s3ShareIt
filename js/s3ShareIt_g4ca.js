ae875e7ee7dd7dd50964bdba25ea6343ca8b776a
bigd1182 <bryant.derrill@gmail.com>
2016-07-08 17:59:11 -0400

    Update s3ShareIt_g4ca.js

--- js/s3ShareIt_g4ca.js
+++ js/s3ShareIt_g4ca.js
@@ -5,11 +5,11 @@ jQuery.noConflict();
     links: null,
     create: function(options) {
       this.generateLinks(options);
-      if ($('#s3ShareIt', $(options.el))) {
-        var el = $('#s3ShareIt', $(options.el));
+      if (sub$('#s3ShareIt', sub$(options.el))) {
+        var el = sub$('#s3ShareIt', sub$(options.el));
       } else {
-        $(options.el).prepend('<div id="s3ShareIt"></div>');
-        var el = $('#s3ShareIt', $(options.el));
+        sub$(options.el).prepend('<div id="s3ShareIt"></div>');
+        var el = sub$('#s3ShareIt', sub$(options.el));
       }
       var html = '';
       for (i in this.links) {
@@ -18,28 +18,28 @@ jQuery.noConflict();
         }
       }
       var minus = parseInt(parseInt(options.thumbWidth) / 3);
-      $(el).css({
+      sub$(el).css({
         'position': 'absolute',
         'top': '230px',
         'left': -minus + 'px',
         'width': options.thumbWidth + 'px',
         'opacity': options.opacity
       }).hover(function() {
-        $(this).stop().fadeTo('fast', 1);
+        sub$(this).stop().fadeTo('fast', 1);
       }, function() {
-        $(this).stop().fadeTo('fast', options.opacity);
+        sub$(this).stop().fadeTo('fast', options.opacity);
       }).append(html).find('a').hover(function() {
-        $(this).stop().animate({
+        sub$(this).stop().animate({
           'marginLeft': '18px'
         }, 'fast');
       }, function() {
-        $(this).stop().animate({
+        sub$(this).stop().animate({
           'marginLeft': '0px'
         }, 500);
       });
       var minTop = 230;
-      $(window).scroll(function() {
-        var position = $(window).scrollTop();
+      sub$(window).scroll(function() {
+        var position = sub$(window).scrollTop();
         if (position < minTop) {
           el.stop().animate({
             'top': 230
@@ -53,20 +53,20 @@ jQuery.noConflict();
     },
     generateLinks: function(options) {
       this.links = {
-        'twitter': '<a target="_blank" title="Twitt it!" href="http://twitter.com/home?status=' + $('title').html() + '+-+' + document.location + '"><img alt="twitter" src="' + options.iconsPath + '/twitter.png"></a>',
+        'twitter': '<a target="_blank" title="Twitt it!" href="http://twitter.com/home?status=' + sub$('title').html() + '+-+' + document.location + '"><img alt="twitter" src="' + options.iconsPath + '/twitter.png"></a>',
         'facebook': '<a target="_blank" title="Share on Facebook" href="http://www.facebook.com/sharer.php?u=' + document.location + '"><img alt="facebook" src="' + options.iconsPath + '/facebook.png"></a>',
         'digg': '<a target="_blank" title="Submit to Digg" href="http://digg.com/submit?phase=2&amp;url=' + document.location + '"><img alt="digg" src="' + options.iconsPath + '/digg.png"></a>',
         'delicius': '<a target="_blank" title="Submit to Delicius" href="http://del.icio.us/post?url=' + document.location + '"><img alt="delicious" src="' + options.iconsPath + '/delicious.png"></a>',
         'stumbleupon': '<a target="_blank" title="Submit to StumpleUpon" href="http://www.stumbleupon.com/submit?url=' + document.location + '"><img alt="stumbleupon" src="' + options.iconsPath + '/stumbleupon.png"></a>',
         'reddit': '<a target="_blank" title="Submit to Reddit" href="http://reddit.com/submit?url=' + document.location + '"><img alt="reddit" src="' + options.iconsPath + '/reddit.png"></a>',
         'technorati': '<a target="_blank" title="Share on Technorati" href="http://technorati.com/faves?add=' + document.location + '"><img alt="technorati" src="' + options.iconsPath + '/technorati.png"></a>',
-        'linkedin': '<a target="_blank" title="Share on LinkedIn" href="http://www.linkedin.com/shareArticle?mini=true&url=' + document.location + '&title=' + $('title').html() + '"><img alt="linkedin" src="' + options.iconsPath + '/linkedin.png"></a>',
+        'linkedin': '<a target="_blank" title="Share on LinkedIn" href="http://www.linkedin.com/shareArticle?mini=true&url=' + document.location + '&title=' + sub$('title').html() + '"><img alt="linkedin" src="' + options.iconsPath + '/linkedin.png"></a>',
         'myspace': '<a target="_blank" title="Share on MySpace" href="http://www.myspace.com/index.cfm?fuseaction=postto&t=&c=&u=' + document.location + '&l="><img alt="facebook" src="' + options.iconsPath + '/myspace.png"></a>',
       }
     }
   };
 
-  S.fn.s3ShareIt = function(options) {
+ sub$fn.s3ShareIt = function(options) {
     var options = options;
     this.each(function() {
       var defaults = {
@@ -84,7 +84,7 @@ jQuery.noConflict();
         'thumbWidth': 48,
         'iconsPath': '/img/s3ShareIt'
       }
-      options = S.extend(true, defaults, options);
+      options =sub$extend(true, defaults, options);
       methods.create(options);
       return this;
     });
